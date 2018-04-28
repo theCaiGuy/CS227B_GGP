57f0c4f40b55b074ddad2463077d49c156f55f19
theCaiGuy <michaelcai09@gmail.com>
2018-04-28 00:22:20 -0700

    Wrote methods for montecarlo and depth charge. TODO: change pure recursion to combination of iterative deepening and time-limited search to prevent timeout

--- src/main/java/org/ggp/base/player/gamer/statemachine/assign4/MGJMonteCarloGamer.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/assign4/MGJMonteCarloGamer.java
@@ -1,6 +1,7 @@
 package org.ggp.base.player.gamer.statemachine.assign4;
 
 import java.util.List;
+import java.util.Random;
 
 import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
 import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
@@ -26,7 +27,8 @@ public final class MGJMonteCarloGamer extends SampleGamer
 	 * a move generated via depth-limited minimax.
 	 */
 
-	private int limit = 6; // level limit
+	private int limit = 3; // level limit
+	private int count = 4; // number of depth charges
 
 	@Override
 	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
@@ -38,15 +40,13 @@ public final class MGJMonteCarloGamer extends SampleGamer
 
 		// vars for role and state
 		Role role = getRole();
-		List<Role> roles = getStateMachine().findRoles();
-		int role_index = roles.indexOf(role);
 		MachineState currentState = getCurrentState();
 
 		// get the list of all possible moves
 		List<Move> moves = getStateMachine().findLegals(role, currentState);
 
 		// Use minimax to determine the best possible next move
-		Move selection = bestMove(role, currentState, moves, role_index, level);
+		Move selection = bestMove(role, currentState, moves, level);
 
 		/*
 		 * get the final time after the move is chosen
@@ -67,12 +67,12 @@ public final class MGJMonteCarloGamer extends SampleGamer
 	 * it finds the moves of the opponents that returns the lowest possible score
 	 * (thereby populating the minnodes).
 	 */
-	private Move bestMove(Role role, MachineState state, List<Move> actions, int role_index, int level) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+	private Move bestMove(Role role, MachineState state, List<Move> actions, int level) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 		Move chosenMove = actions.get(0);
 		int score = 0;
 		// loop through all actions and find the best score and return this
 		for (int i = 0; i < actions.size(); i++) {
-			int result = minScore(role, actions.get(i), state, role_index, level);
+			int result = minScore(role, actions.get(i), state, level);
 			if (result > score) {
 				score = result;
 				chosenMove = actions.get(i);
@@ -87,13 +87,13 @@ public final class MGJMonteCarloGamer extends SampleGamer
 	 * in the roles array, calculates the minimum score out
 	 * of all possible joint actions conducted by the opponents.
 	 */
-	private int minScore(Role role, Move move, MachineState state, int role_index, int level) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+	private int minScore(Role role, Move move, MachineState state, int level) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 		int score = 100;
 		List<List<Move>> allJointActions = getStateMachine().getLegalJointMoves(state, role, move);
 		// go through all possible combinations of actions for opponents and return worst outcome
 		for (int i = 0; i < allJointActions.size(); i++) {
 			MachineState updatedState = getStateMachine().findNext(allJointActions.get(i), state);
-			int result = maxScore(role, updatedState, role_index, level + 1);
+			int result = maxScore(role, updatedState, level + 1);
 			if (result < score) {
 				score = result;
 			}
@@ -107,17 +107,18 @@ public final class MGJMonteCarloGamer extends SampleGamer
 	 * in the roles array, finds the highest
 	 * scoring move and returns its score.
 	 */
-	private int maxScore(Role role, MachineState state, int role_index, int level) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+	private int maxScore(Role role, MachineState state, int level) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 		// if in a terminal state or exceeds the level limit, return, otherwise recursively find all terminal results
 		if (getStateMachine().findTerminalp(state)) {
 			return getStateMachine().findReward(role, state);
-		} else if (level >= limit) return 0;
+		} else if (level >= limit) return montecarlo(role, state);
 		else {
 			// find actions in this case and return the highest score found amongst them
 			List<Move> actions = getStateMachine().findLegals(role, state);
 			int score = 0;
 			for (int i = 0; i < actions.size(); i++) {
-				int result = minScore(role, actions.get(i), state, role_index, level);
+				int result = minScore(role, actions.get(i), state, level);
+				if (result == 100) return 100;
 				if (result > score) {
 					score = result;
 				}
@@ -132,8 +133,12 @@ public final class MGJMonteCarloGamer extends SampleGamer
 	 * found from each depth charge and returns the average
 	 * of those scores
 	 */
-	private int montecarlo(Role role, MachineState state, int count) {
-		return 0;
+	private int montecarlo(Role role, MachineState state) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		int total = 0;
+		for (int i = 0; i < count; i++) {
+			total = total + depthcharge(role, state);
+		}
+		return total/count;
 	}
 
 	/*
@@ -142,13 +147,14 @@ public final class MGJMonteCarloGamer extends SampleGamer
 	 * move until a terminal state is reached, then returns
 	 * the reward received at said terminal state
 	 */
-	private int depthcharge(Role role, MachineState state) {
-		return 0;
+	private int depthcharge(Role role, MachineState state) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		if (getStateMachine().findTerminalp(state)) {
+			return getStateMachine().findReward(role, state);
+		}
+		List<Move> actions = getStateMachine().findLegals(role,  state);
+		Random rand = new Random();
+		List<List<Move>> allJointActions = getStateMachine().getLegalJointMoves(state, role, actions.get(rand.nextInt(actions.size())));
+		MachineState updated_state = getStateMachine().findNext(allJointActions.get(rand.nextInt(allJointActions.size())), state);
+		return depthcharge(role, updated_state);
 	}
-
-
-
-
-
-
 }

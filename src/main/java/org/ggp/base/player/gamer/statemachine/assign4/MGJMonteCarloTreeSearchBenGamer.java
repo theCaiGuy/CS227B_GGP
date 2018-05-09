d7390b4d48c3e79678716f2768bab9f46a0eb79c
theCaiGuy <michaelcai09@gmail.com>
2018-05-09 12:43:02 -0700

    Few changes to MCTS, no changes to other thing

--- src/main/java/org/ggp/base/player/gamer/statemachine/assign4/MGJMonteCarloTreeSearchBenGamer.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/assign4/MGJMonteCarloTreeSearchBenGamer.java
@@ -32,7 +32,7 @@ public final class MGJMonteCarloTreeSearchBenGamer extends SampleGamer
 
 	private long time_lim = 3000; // time limit
 	private long absolute_lim = 2500;
-	private int count = 6; //num depth charges
+	private int count = 4; //num depth charges
 
 	// Class to represent Node in search tree
 	public class Node {
@@ -67,20 +67,21 @@ public final class MGJMonteCarloTreeSearchBenGamer extends SampleGamer
 		long start = System.currentTimeMillis();
 
 		// vars for role and state
+		StateMachine machine = getStateMachine();
 		Role role = getRole();
-		List<Role> roles = getStateMachine().findRoles();
+		List<Role> roles = machine.findRoles();
 		int roleIdx = roles.indexOf(role);
 		MachineState currentState = getCurrentState();
 
 		// get the list of all possible moves
-		List<Move> moves = getStateMachine().findLegals(role, currentState);
+		List<Move> moves = machine.findLegals(role, currentState);
 
 		// if noop or only one possible move return immediately
 		if (moves.size() == 1) return moves.get(0);
 
 		Node root = new Node(null, null, getCurrentState(), true);
 		// Use Monte Carlo Tree Search to determine the best possible next move
-		Move selection = bestMove(root, role, start, timeout, roleIdx);
+		Move selection = bestMove(root, role, start, timeout, roleIdx, machine);
 
 		/*
 		 * get the final time after the move is chosen
@@ -97,15 +98,15 @@ public final class MGJMonteCarloTreeSearchBenGamer extends SampleGamer
 
 	/* while still have time repeatedly update and search the tree
 	 */
-	private Move bestMove(Node root, Role role, long start, long timeout, int roleIdx) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+	private Move bestMove(Node root, Role role, long start, long timeout, int roleIdx, StateMachine machine) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 		while (timeout - System.currentTimeMillis() >= time_lim) {
-			Node selNode = select(root);
+			Node selNode = select(root, machine);
 			int score = 0;
-			if (getStateMachine().findTerminalp(selNode.currentState)) {
-				score = getStateMachine().findReward(role, selNode.currentState);
+			if (machine.findTerminalp(selNode.currentState)) {
+				score = machine.findReward(role, selNode.currentState);
 			} else {
-				expand(selNode, role);
-				score = montecarlo(role, selNode, timeout);
+				expand(selNode, role, machine);
+				score = montecarlo(role, selNode, timeout, machine);
 			}
 			backpropagate(selNode, score);
 		}
@@ -114,6 +115,11 @@ public final class MGJMonteCarloTreeSearchBenGamer extends SampleGamer
 		Move bestMove = root.children.get(0).move.get(roleIdx);
 		double bestUtility =  root.children.get(0).utility;
 		for (Node child : root.children) {
+//			if (machine.findTerminalp(child.currentState)) {
+//				if (machine.findReward(role, child.currentState) == 100) {
+//					return child.move.get(roleIdx);
+//				}
+//			}
 			if (child.utility >= bestUtility) {
 				bestUtility = child.utility;
 				bestMove = child.move.get(roleIdx);
@@ -122,8 +128,8 @@ public final class MGJMonteCarloTreeSearchBenGamer extends SampleGamer
 		return bestMove;
 	}
 
-	private Node select(Node node) {
-		if (getStateMachine().findTerminalp(node.currentState)) {
+	private Node select(Node node, StateMachine machine) {
+		if (machine.findTerminalp(node.currentState)) {
 			return node;
 		}
 		if (node.visits == 0) {
@@ -141,7 +147,7 @@ public final class MGJMonteCarloTreeSearchBenGamer extends SampleGamer
 					result = child;
 				}
 			}
-			return select(result);
+			return select(result, machine);
 		}
 	}
 
@@ -149,38 +155,54 @@ public final class MGJMonteCarloTreeSearchBenGamer extends SampleGamer
 		return (int) (node.utility / node.visits + Math.sqrt(2 * Math.log(node.parent.visits) / node.visits));
 	}
 
-	private void expand(Node node, Role role) throws MoveDefinitionException, TransitionDefinitionException {
-//		if (getStateMachine().findTerminalp(node.currentState)) {
-//			return;
-//		}
-		List<Move> actions = getStateMachine().findLegals(role, node.currentState);
+	private void expand(Node node, Role role, StateMachine machine) throws MoveDefinitionException, TransitionDefinitionException {
+		List<Move> actions = machine.findLegals(role, node.currentState);
 		for (Move action : actions) {
-			List<List<Move>> allJointActions = getStateMachine().getLegalJointMoves(node.currentState, role, action);
+			List<List<Move>> allJointActions = machine.getLegalJointMoves(node.currentState, role, action);
 			for (List<Move> jointActions : allJointActions) {
-				MachineState newState = getStateMachine().findNext(jointActions, node.currentState);
+				MachineState newState = machine.findNext(jointActions, node.currentState);
 				Node newnode = new Node(node, jointActions, newState, false);
 				node.children.add(newnode);
 			}
 		}
 	}
 
+//	private void backpropagate(Node node, int score) {
+//		node.visits++;
+//		node.utility = node.utility + score;
+//		if (node.parent != null) {
+//			backpropagate(node.parent, score);
+//		}
+//	}
+
+	/*
+	 * Backpropogates a found score to parent nodes until the root is reached
+	 */
 	private void backpropagate(Node node, int score) {
-		node.visits++;
+		Node curr_node = node;
+		node.visits += 1;
 		node.utility = node.utility + score;
-		if (node.parent != null) {
-			backpropagate(node.parent, score);
+		while (curr_node.parent != null) {
+			curr_node = curr_node.parent;
+			curr_node.visits += 1;
+			curr_node.utility += score;
 		}
 	}
 
-
-	private int montecarlo(Role role, Node curr_node, long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+	/*
+	 * Manages depth charges for a monte carlo search
+	 */
+	private int montecarlo(Role role, Node curr_node, long timeout, StateMachine machine) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 		int total = 0;
 		for (int i = 0; i < count; i++) {
-			total = total + depthcharge(role, curr_node, timeout, getStateMachine());
+			total = total + depthcharge(role, curr_node, timeout, machine);
 		}
 		return total / count;
 	}
 
+	/*
+	 * Performs a depth charge by searching for a terminal state
+	 */
 	private int depthcharge(Role role, Node curr_node, long timeout, StateMachine m) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
 		Random random = new Random();
 		MachineState curr_state = curr_node.currentState;
@@ -192,35 +214,5 @@ public final class MGJMonteCarloTreeSearchBenGamer extends SampleGamer
 		return m.findReward(role,  curr_state);
 	}
 
-//	/*
-//	 * This function manages the depth charges extending
-//	 * from a particular node. It adds up the resulting scores
-//	 * found from each depth charge and returns the average
-//	 * of those scores
-//	 */
-//	private int montecarlo(Role role, MachineState state, long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
-//		int total = 0;
-//		for (int i = 0; i < count; i++) {
-//			total = total + depthcharge(role, state, timeout, getStateMachine());
-//		}
-//		return total/count;
-//	}
-//
-//	/*
-//	 * This function performs a depth charge on the given
-//	 * game tree. At each state, it randomly chooses one
-//	 * move until a terminal state is reached, then returns
-//	 * the reward received at said terminal state
-//	 */
-//	private int depthcharge(Role role, MachineState state, long timeout, StateMachine m) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
-//		Random random = new Random();
-//		MachineState current = state;
-//		while (!m.findTerminalp(current)) {
-//			List<List<Move>> moves = m.getLegalJointMoves(current);
-//			current = m.getNextState(current, moves.get(random.nextInt(moves.size())));
-//			if (timeout - System.currentTimeMillis() < absolute_lim) return 0;
-//		}
-//		return m.findReward(role, current);
-//	}
 
 }
